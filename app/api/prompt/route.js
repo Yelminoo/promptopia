7d9effb1e93b9bd9cca47f8ef6d5d60c95656d8c
Yelminoo <yelminoo2015@gmail.com>
2024-04-29 00:13:49 +0630

    caching fix

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,9 +5,13 @@ export const GET = async (request) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
-
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    const headers = {
+      "Cache-Control": "no-store, no-cache, must-revalidate",
+      "Content-Type": "application/json",
+    };
+    return new Response(JSON.stringify(prompts), { status: 200, headers });
   } catch (error) {
+    console.log(error + "error");
     return new Response("Failed to fetch", { status: 500 });
   }
 };
